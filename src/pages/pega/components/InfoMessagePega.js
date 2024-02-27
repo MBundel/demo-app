a146fa1013f6f12b76e5376854a8ddfca3b784dd
MBundel <michel@bundel.eu>
2024-02-27 15:59:32 +0100

    change inline height

--- src/pages/pega/components/InfoMessagePega.js
+++ src/pages/pega/components/InfoMessagePega.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const InfoMessagePega = ({ message, isVisible }) => {
-  return <>{isVisible ? <h2>{message} </h2> : ""}</>;
+  return <>{isVisible ? <h2 style={{lineHeight: "1"}}>{message} </h2> : ""}</>;
 };
 
 export default InfoMessagePega;
