15b592f4bc09a0f10e260460c281a22ac16a1a23
MBundel <michel@bundel.eu>
2024-02-27 16:03:23 +0100

    refactoring style, fix inlineheight

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../styles/styleLogin.scss";
+import "./login.scss";
 import "../../styles/styles.scss";
 import FormHeader from "./components/FormHeader";
 // import { Link } from 'react-router-dom';
