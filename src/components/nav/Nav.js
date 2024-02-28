f7854094846da810ddeafbccc26f458bb3145782
MBundel <michel@bundel.eu>
2024-02-28 10:00:56 +0100

    refactor scss dir structure

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,53 +1,55 @@
-import   { React, useState, useEffect }from "react";
+import { React, useState, useEffect } from "react";
 import { uniqa, allianz, zurich } from "../../records/insuranceFirms";
-import '../../styles/styles.scss'
- 
-  import { Link, useLocation  } from 'react-router-dom';
+import "./nav.scss";
 
+import { Link, useLocation } from "react-router-dom";
 
-
-function Navbar({selectedFirmName}) {
-
-  const [currentPath, setCurrentPath] = useState('');
+function Navbar({ selectedFirmName }) {
+  const [currentPath, setCurrentPath] = useState("");
   const location = useLocation();
 
-
- 
   const firms = { uniqa, allianz, zurich };
   const seF = selectedFirmName;
 
-
   // -------------------------------------------------------------
   const selectedFirm = firms[seF];
-  
-  
-console.log( firms[seF].name + " --------------------------------------------")
 
-   useEffect(() => {
+  useEffect(() => {
     setCurrentPath(location.pathname);
   }, [location.pathname]);
 
   return (
     <>
-     <div className="homepage-container">
-     <header role="banner" className="header-box">
-<a href="http://localhost:3000">
-<img src={`${selectedFirm.imageSrc}`} alt="Logo" className="logo"/>
-</a>
-<ul className="upper-nav">
-    {/* <li><a href="#privat">Privat</a></li>        |
+      <div className="homepage-container">
+        <header role="banner" className="header-box">
+          <a href="http://localhost:3000">
+            <img src={`${selectedFirm.imageSrc}`} alt="Logo" className="logo" />
+          </a>
+          <ul className="upper-nav">
+            {/* <li><a href="#privat">Privat</a></li>        |
     <li><a href="#unternehmen">Unternehmen</a></li> */}
-  
-    <li><a href="#suche"><span className="resp">Suche</span></a></li>
-    {currentPath === '/pega' ? (
-          <li><Link to="/login"><span className="resp">Logout</span></Link></li>
-        ) : (
-          <li><Link to="/login"><span className="resp">Login</span></Link></li>
-        )}
-</ul>
 
+            <li>
+              <a href="#suche">
+                <span className="resp">Suche</span>
+              </a>
+            </li>
+            {currentPath === "/pega" ? (
+              <li>
+                <Link to="/login">
+                  <span className="resp">Logout</span>
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <Link to="/login">
+                  <span className="resp">Login</span>
+                </Link>
+              </li>
+            )}
+          </ul>
 
-{/* <nav className="nav">
+          {/* <nav className="nav">
 
     <ul>
         <li><a href="#gesundheit">Gesundheit</a></li>
@@ -62,9 +64,8 @@ console.log( firms[seF].name + " --------------------------------------------")
 
     </ul>
 </nav> */}
-
-</header>
-</div>
+        </header>
+      </div>
     </>
   );
 }
