7406af8ba4e6e5b18a426e6b1f3336e3b9926c8e
MBundel <michel@bundel.eu>
2024-02-28 10:01:16 +0100

    del unnessessary comments

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -26,8 +26,7 @@ function Navbar({ selectedFirmName }) {
             <img src={`${selectedFirm.imageSrc}`} alt="Logo" className="logo" />
           </a>
           <ul className="upper-nav">
-            {/* <li><a href="#privat">Privat</a></li>        |
-    <li><a href="#unternehmen">Unternehmen</a></li> */}
+   
 
             <li>
               <a href="#suche">
@@ -49,21 +48,7 @@ function Navbar({ selectedFirmName }) {
             )}
           </ul>
 
-          {/* <nav className="nav">
 
-    <ul>
-        <li><a href="#gesundheit">Gesundheit</a></li>
-        <li><a href="#kfz">Kfz</a></li>
-        <li><a href="#wohnen">Wohnen</a></li>
-        <li><a href="#reise">Reise</a></li>
-        <li><a href="#vorsorge">Vorsorge</a></li>
-        <li><a href="#unfall">Unfall</a></li>
-        <li><a href="#rechtsschutz">Rechtsschutz</a></li>
-        <li><a href="#freizeit">Freizeit</a></li>
-        <li><a href="#kunst">Kunst</a></li>
-
-    </ul>
-</nav> */}
         </header>
       </div>
     </>
