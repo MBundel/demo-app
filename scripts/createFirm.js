bf07c5ecc20b008e1ea237ef89c21b274ff560fd
MBundel <michel@bundel.eu>
2024-04-10 11:34:38 +0200

    create new firm automation script

--- scripts/createFirm.js
+++ scripts/createFirm.js
@@ -1,38 +1,76 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const { promisify } = require('util');
-const mkdir = promisify(fs.mkdir);
-const writeFile = promisify(fs.writeFile);
+const inquirer = require("inquirer");
+const fs = require("fs").promises;
 
-
-
-const fileNames = ['_button.scss', '_colorScheme.scss', '_container.scss', '_general.scss', '_style.scss'];
-const styleContent = `@import "./button";
-@import "./container";
-@import "./general";`;
-
-inquirer.prompt([{
-    type: 'input',
-    name: 'name',
-    message: 'Wie soll die Firma heißen?',
-}]).then(async (answers) => {
-    const folderName = answers.name;
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Wie soll die Firma heißen?",
+    },
+  ])
+  .then(async (answers) => {
+    const folderName = formatFileName(answers.name);
     const folderPath = `src/styles/${folderName}`;
+    const {
+      buttonContent,
+      colorSchemeContent,
+      containerContent,
+      generalContent,
+      styleContent,
+    } = require("./records/firmFileContent.js")(folderName);
 
-    if (!fs.existsSync(folderPath)) {
-        await mkdir(folderPath, { recursive: true });
-    }
+    const fileNameToContent = {
+      "_button.scss": buttonContent,
+      "_colorScheme.scss": colorSchemeContent,
+      "_container.scss": containerContent,
+      "_general.scss": generalContent,
+      "_style.scss": styleContent,
+    };
 
-    for (let file of fileNames) {
-        let content = '';
-        if (file === '_style.scss') {
-            content = styleContent;
-        }
-        await writeFile(`${folderPath}/${file}`, content);
-        console.log(`${file} wurde erfolgreich erstellt.`);
-    }
+    fs.access(folderPath)
+        .then(async () => {
+            // Ordner existiert bereits
+        })
+        .catch(async () => {
+          // Ordner existiert nicht, erstelle ihn
+          await fs.mkdir(folderPath, { recursive: true });
 
-    console.log(`Alles erledigt! Ordner ${folderPath} wurde mit SCSS-Dateien erstellt.`);
-}).catch(error => {
-    console.error('Konnte Ordner / Dateien nicht erstellen:', error);
-});
+          for (let file in fileNameToContent) {
+            const content = fileNameToContent[file];
+            await fs.writeFile(`${folderPath}/${file}`, content);
+            console.log(`${file} wurde erfolgreich erstellt.`);
+          }
+
+          console.log(
+            `Alles erledigt! Ordner ${folderPath} wurde mit SCSS-Dateien erstellt.`
+          );
+          addScssImport(folderName);
+        });
+  })
+  .catch((error) => {
+    console.error("Konnte Ordner / Dateien nicht erstellen:", error);
+  });
+
+const formatFileName = (name) =>
+  name
+    .toLowerCase()
+    .replace(/ä/g, "ae")
+    .replace(/ö/g, "oe")
+    .replace(/ü/g, "ue")
+    .replace(/ß/g, "ss")
+    .replace(/\s(\w)/g, (match) => match.toUpperCase())
+    .replace(/[\s\W]/g, "");
+
+const addScssImport = async (companyName) => {
+  const scssFile = "src/styles/app.scss";
+  const importLine = `@use "./${companyName}/style" as ${companyName};\n`;
+
+  try {
+    const data = await fs.readFile(scssFile, "utf8");
+    await fs.writeFile(scssFile, importLine + data);
+    console.log(`Eine neue Zeile wurde zur Datei ${scssFile} hinzugefügt.`);
+  } catch (err) {
+    console.error(err);
+  }
+};
--- scripts/records/firmFileContent.js
+++ scripts/records/firmFileContent.js
@@ -1,29 +1,120 @@
-export const buttonContent = `@forward "./colorScheme.scss";`;
+module.exports = (folderName) => {
+  return {
+    buttonContent: `
+    @forward "./colorScheme.scss";
+    @mixin button_default {
+      color: $${folderName}Grey;
+      font-size: 15px;
+      font-weight: 900;
+      font-family: inherit;
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      border: none;
+      border-radius: 5px 0 0 5px  ;
+      padding: 5px 5px;
+      width: 200px;
+      height: 45px;
+      margin: 10px auto;
+      justify-content: center;
+      cursor: pointer;
+    
+      @media screen and (max-width:768px) {
+        // min-width: 220px;
+      }
+    
+      &:hover {
+        background-color: $${folderName}White;
+        color: $${folderName}Blue;
+      }
+    }
+    
+    @mixin button_regular {
+      @include button_default;
+      background-color: $${folderName}Blue;
+     
+    }
+    
+    @mixin button_strong {
+      @include button_default;
+      background-color: $${folderName}Yellow;
+      width: auto;
+     
+    }
+    
+    @mixin button_deactivated {
+      @include button_default;
+      background-color: $${folderName}Grey;  
+    }
+    
+    @mixin buttonLogin{
+      @include button_regular;
+    }
+    @mixin buttonNewReg{
+      @include button_regular;
+    }
+    `,
 
+    colorSchemeContent: `
+$${folderName}Blue:             #7C90DB;
+$${folderName}LightBlue:        #EEF2FA;
+$${folderName}White:            #FFFFFF;
+$${folderName}Grey:             #A8AEC1;
+$${folderName}DarkGrey:         #646875;
+$${folderName}Green:            #008000;
+$${folderName}LightGreen:       #91F5AD;
+$${folderName}Red:              #D56062;
+$${folderName}LightRed:         #FFA07A;
+$${folderName}Yellow:           #ECC30B;
+$${folderName}LightYellow:      #FFFFE0;
+$${folderName}Purple:           #51344D;
+$${folderName}LightPurple:      #700548;
+$${folderName}Pink:             #D81159;
+$${folderName}LightPink:        #FFD3DA;
+$${folderName}Black:            #0D160B;
+`,
 
+    containerContent: `
+    @forward "./colorScheme.scss";
+    @mixin mainLoginContent{
+      color: $${folderName}DarkGrey;
+      font-family: "Segoe UI", "Roboto", Helvetica, Arial, serif;
+      background-color: $${folderName}White;
+      justify-content: space-between;
+      box-shadow: 1px 1px 5px -4.5px #333333;
+  
+  }
+  
+  @mixin pega_container{
+      background-color: $${folderName}White;
+  }
+    `,
 
+    generalContent: `
+    @forward "./colorScheme.scss";
+    @mixin a{
 
-export const colorSchemeContent = 
-`$_firm_Blue:             #005CA9;
- $_firm_LightBlue:        #EEF2FA;
- $_firm_White:            #FFFFFF;
- $_firm_Grey:             #617384;
- $_firm_DarkGrey:         #1b1b1b;`;
+      & a:visited {
+           color: $${folderName}Blue;
+         }
+     }
+  
+  
+  @mixin style_default{
+      @include a;
+      color: $${folderName}White;
+      background-color: $${folderName}Grey;
+      padding-bottom: 350px;
+      font-size:  20px ;
+     
+      font-family: "Calibri", sans-serif;
+  
+      
+  }
+    `,
 
-
-
-
-export const containerContent = `@forward "./colorScheme.scss";`;
-
-
-
-
-export const generalContent = `@forward "./colorScheme.scss";`;
-
-
-
-
-export const styleContent = 
-`@import "./button";
+    styleContent: `
+@import "./button";
 @import "./container";
-@import "./general";`;
+@import "./general";`,
+  };
+};
